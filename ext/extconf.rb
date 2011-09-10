b80d441036af9be98589d7d6ac0baa4a8088e583
Antonio Terceiro <terceiro@softwarelivre.org>
2011-09-09 17:38:44 -0700

    Embed the R directory in the compiled extension
    If the user already informed the R directory while compiling the
    extention, then it is not needed to explictly set R_HOME before using
    the library.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -10,4 +10,12 @@ unless have_header("R.h")
   exit 1
 end
 
+File.open("config.h", "w") do |f|
+  f.puts("#ifndef R_CONFIG_H")
+  f.puts("#define R_CONFIG_H")
+  r_home = $configure_args.has_key?('--with-R-dir') ? $configure_args['--with-R-dir'].inspect : 'NULL'
+  f.puts("#define RSRUBY_R_HOME #{r_home}")
+  f.puts("#endif")
+end
+
 create_makefile("rsruby_c")
