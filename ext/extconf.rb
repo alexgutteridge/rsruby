730f438d04371f4f0448153fbe931feabed804dc
Mechaferret <mechaferret@Monica-McArthurs-MacBook-Pro.local>
2011-11-20 11:25:25 -0800

    Modify extconf.rb to look for R lib and headers in common directories as well as config (similar to mysql2 gem)

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,10 +1,26 @@
 require 'mkmf'
 
 dir_config('R')
+
+some_paths = ENV['PATH'].split(File::PATH_SEPARATOR) + %w[
+  /usr/local/lib64/R
+  /usr/local/lib/R 
+  /usr/lib64/R 
+  /usr/lib/R 
+  /Library/Frameworks/R.framework/Resources
+]
+
+some_lib_paths = some_paths.map{|dir| "#{dir}/lib" }
+find_library('R', nil, *some_lib_paths)
+
 unless have_library("R")
   $stderr.puts "\nERROR: Cannot find the R library, aborting."
   exit 1
 end
+
+some_include_paths = some_paths.map{|dir| "#{dir}/include" } + %w[/usr/include/R]
+find_header('R.h', nil, *some_include_paths)
+
 unless have_header("R.h")
   $stderr.puts "\nERROR: Cannot find the R header, aborting."
   exit 1
